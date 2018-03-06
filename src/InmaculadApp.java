68c59736ecb9e0ab86e19fd960b9e3f1233fc92f
cTatu <cristian.tatu@estudiante.uam.es>
2018-03-06 10:48:56 +0100

    añadir clases

--- src/InmaculadApp.java
+++ src/InmaculadApp.java
@@ -1,4 +1,14 @@
+import java.awt.List;
 
 public class InmaculadApp {
-
+	
+	List<Usuario> usuarios; 
+	
+	public InmaculadApp() {
+		
+	}
+	
+	public void cargarUsuario() {
+		
+	}
 }
---
+++ src/Usuario.java
@@ -0,0 +1,11 @@
+
+public abstract class Usuario {
+	private String nombre, NIF, apellido, tarjetaCredito;
+	private String 
+	public Usuario(String nombre, String NIF, String appelido, String tarjetaCredito) {
+		this.nombre = nombre;
+		this.NIF = NIF;
+		this.apellido = apellido;
+		this.tarjetaCredito = tarjetaCredito;
+	}
+}
