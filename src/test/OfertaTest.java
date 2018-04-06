7d58643888ded6604a26ee7c77479578a223b63a
David <David@192.168.56.1>
2018-04-06 17:06:32 +0200

    test reserva

---
+++ src/test/OfertaTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author David
+ *
+ */
+public class OfertaTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link oferta.Oferta#añadirRectificacion(java.util.Map)}.
+	 */
+	@Test
+	public void testAñadirRectificacion() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link oferta.Oferta#mostrarRectificaciones()}.
+	 */
+	@Test
+	public void testMostrarRectificaciones() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link oferta.Oferta#contratar(cliente.Cliente)}.
+	 */
+	@Test
+	public void testContratar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link oferta.Oferta#añadirOpinion(opinion.Opinion)}.
+	 */
+	@Test
+	public void testAñadirOpinion() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link oferta.Oferta#calcularMedia()}.
+	 */
+	@Test
+	public void testCalcularMedia() {
+		fail("Not yet implemented");
+	}
+
+}
